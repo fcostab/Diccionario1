224c906deda1fc1a664cc7459ba7dbb372b77a21
fcostab <francocostab@gmail.com>
2024-05-09 21:09:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             "haker" : "alguien que te odia o que te tiene envidia ",
-            "BRO" : "Mejor amigo/persona a la que le tienes confianza"
+            "BRO" : "Mejor amigo/persona a la que le tienes confianza",
+            "CRINGE" : "Algo raro o embarazoso",
+            "SHEESH" : "ligera desaprobacion"
             }
+print("Bienvenido al diccionario de las palabras nuevas ")
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ") 
-
 if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[work])
 else:
-    # ¿Qué hacer si no se encuentra la palabra?
+    print("lo lamento pero no tenemos esa palabra pero lo actualizaremos")
